a9472637bf8074d6d1e8f910351c91c763459aba
Ryan Sponzilli <ryguy2k4@gmail.com>
2023-01-09 21:52:35 -0600

    Started working on adding a preset editor

--- DeepSkyCatalog/Views/SettingsViews/GearSettings.swift
+++ DeepSkyCatalog/Views/SettingsViews/GearSettings.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct GearSettings: View {
     @FetchRequest(sortDescriptors: [SortDescriptor(\ImagingPreset.name, order: .forward)]) var presetList: FetchedResults<ImagingPreset>
     @Environment(\.managedObjectContext) var context
+    @State private var presetCreatorModal: Bool = false
     @State private var presetEditorModal: Bool = false
     
     var body: some View {
@@ -28,11 +29,26 @@ struct GearSettings: View {
                                     Label("Delete", systemImage: "trash")
                                 }
                             }
+                            Button {
+                                presetEditorModal = true
+                            } label: {
+                                Label("Edit", systemImage: "pencil")
+                            }
+                            .tint(.yellow)
+
                         }
                         .foregroundColor(.primary)
+                        .sheet(isPresented: $presetCreatorModal) {
+                            ImagingPresetCreator()
+                                .presentationDetents([.fraction(0.8)])
+                        }
+                        .sheet(isPresented: $presetEditorModal) {
+                            ImagingPresetEditor(preset: preset)
+                                .presentationDetents([.fraction(0.8)])
+                        }
                 }
                 // Button for adding a new location
-                Button(action: { presetEditorModal = true }) {
+                Button(action: { presetCreatorModal = true }) {
                     HStack {
                         Image(systemName: "plus.circle")
                         Text("Add Preset")
@@ -41,17 +57,13 @@ struct GearSettings: View {
                 }
             }
         }
-        .sheet(isPresented: $presetEditorModal) {
-            ImagingPresetEditor()
-                .presentationDetents([.fraction(0.8)])
-        }
         .onDisappear {
             PersistenceManager.shared.saveData(context: context)
         }
     }
 }
 
-struct ImagingPresetEditor: View {
+struct ImagingPresetCreator: View {
     @Environment(\.managedObjectContext) var context
     @Environment(\.dismiss) var dismiss
     @FocusState var isInputActive: Bool
@@ -71,13 +83,13 @@ struct ImagingPresetEditor: View {
                         // Fields to manually enter gear info
                         SettingsField(text: $name, label: "Name: ", keyboardType: .default)
                             .focused($isInputActive)
-                        SettingsField(text: $focalLengthText, label: "Focal Length: ")
+                        SettingsField(text: $focalLengthText, label: "Focal Length (mm): ")
                             .focused($isInputActive)
-                        SettingsField(text: $pixelSizeText, label: "Pixel Size: ")
+                        SettingsField(text: $pixelSizeText, label: "Pixel Size (µm): ")
                             .focused($isInputActive)
-                        SettingsField(text: $resolutionLength, label: "Resolution Length: ")
+                        SettingsField(text: $resolutionLength, label: "Resolution Length (px): ")
                             .focused($isInputActive)
-                        SettingsField(text: $resolutionWidth, label: "Resolution Width: ")
+                        SettingsField(text: $resolutionWidth, label: "Resolution Width (px): ")
                             .focused($isInputActive)
                     }
                 }
@@ -102,6 +114,70 @@ struct ImagingPresetEditor: View {
         }
     }
 }
+
+struct ImagingPresetEditor: View {
+    @Environment(\.managedObjectContext) var context
+    @Environment(\.dismiss) var dismiss
+    @FocusState var isInputActive: Bool
+    
+    let preset: ImagingPreset
+    @State private var name: String
+    @State private var focalLengthText: String
+    @State private var pixelSizeText: String
+    @State private var resolutionLength: String
+    @State private var resolutionWidth: String
+    
+    init(preset: ImagingPreset) {
+        self._name = State(initialValue: preset.name ?? "")
+        self._focalLengthText = State(initialValue: String(preset.focalLength))
+        self._pixelSizeText = State(initialValue: String(preset.pixelSize))
+        self._resolutionLength = State(initialValue: String(preset.resolutionLength))
+        self._resolutionWidth = State(initialValue: String(preset.resolutionWidth))
+        self.preset = preset
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    ConfigSection {
+                        // Fields to manually enter gear info
+                        SettingsField(text: $name, label: "Name: ", keyboardType: .default)
+                            .focused($isInputActive)
+                        SettingsField(text: $focalLengthText, label: "Focal Length (mm): ")
+                            .focused($isInputActive)
+                        SettingsField(text: $pixelSizeText, label: "Pixel Size (µm): ")
+                            .focused($isInputActive)
+                        SettingsField(text: $resolutionLength, label: "Resolution Length (px): ")
+                            .focused($isInputActive)
+                        SettingsField(text: $resolutionWidth, label: "Resolution Width (px): ")
+                            .focused($isInputActive)
+                    }
+                }
+                // Display Pixel Scale and FOV Size
+                let pixelScale = (Double(pixelSizeText) ?? .nan) / (Double(focalLengthText) ?? .nan) * 206.2648
+                let fovLength = pixelScale * (Double(resolutionLength) ?? .nan) / 60
+                let fovWidth = pixelScale * (Double(resolutionWidth) ?? .nan) / 60
+                Text("Pixel Scale: \(pixelScale)")
+                Text("FOV: \(fovLength)' x \(fovWidth)'")
+                Spacer()
+            }
+            .toolbar {
+                KeyboardDismissButton(isInputActive: _isInputActive)
+                ToolbarItemGroup(placement: .confirmationAction) {
+                    Button("Save") {
+                        PersistenceManager.shared.addImagingPreset(name: name, focalLength: Double(focalLengthText) ?? .nan, pixelSize: Double(pixelSizeText) ?? .nan, resLength: Int16(resolutionLength) ?? 1920, resWidth: Int16(resolutionWidth) ?? 1080, isSelected: preset.isSelected, context: context)
+                        context.delete(preset)
+                        PersistenceManager.shared.saveData(context: context)
+                        dismiss()
+                    }
+                }
+            }
+            .padding(0)
+        }
+    }
+}
+
 
 //struct GearSettings_Previews: PreviewProvider {
 //    static var previews: some View {
