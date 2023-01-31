ef97f62ede92a1b05f1683a5c209d390d0a49b41
Ryan Sponzilli <ryguy2k4@gmail.com>
2023-01-30 22:46:18 -0600

    Fixed crash.

--- DeepSkyCatalog/Views/TargetCatalogs/CatalogList.swift
+++ DeepSkyCatalog/Views/TargetCatalogs/CatalogList.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct CatalogList: View {
     @FetchRequest(sortDescriptors: [SortDescriptor(\SavedLocation.isSelected, order: .reverse)]) var locationList: FetchedResults<SavedLocation>
-    @FetchRequest(sortDescriptors: []) var reportSettings: FetchedResults<ReportSettings>
+    @FetchRequest(sortDescriptors: []) var targetSettings: FetchedResults<TargetSettings>
     let catalog: TargetCatalog
     var body: some View {
         List(DeepSkyTargetList.allTargets.filteredByCatalog([catalog]).sortedByCatalog(catalog: catalog).reversed()) { target in
             NavigationLink {
                 DetailView(target: target)
                     .environmentObject(locationList.first!)
-                    .environmentObject(reportSettings.first!)
+                    .environmentObject(targetSettings.first!)
             } label: {
                 HStack {
                     Text("#\(target.designation.first(where: {$0.catalog == catalog})!.number)")
