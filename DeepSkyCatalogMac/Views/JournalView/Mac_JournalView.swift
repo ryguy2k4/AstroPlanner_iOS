0be044844956907e988a50941ef2c3a39e8ca6ba
Ryan Sponzilli <ryguy2k4@gmail.com>
2023-10-11 20:20:19 -0500

    Experimentation with Moon calculations. Adding JournalView to Mac. Various aesthetic changes.

---
+++ DeepSkyCatalogMac/Views/JournalView/Mac_JournalView.swift
@@ -0,0 +1,18 @@
+//
+//  Mac_JournalView.swift
+//  DeepSkyCatalogMac
+//
+//  Created by Ryan Sponzilli on 10/3/23.
+//
+
+import SwiftUI
+
+struct Mac_JournalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mac_JournalView()
+}
